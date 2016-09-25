735f0d82fa101ab148cc84cb54f0172d90a2b05c
Taylor Goolsby <tjgoolsby@ucdavis.edu>
2016-09-24 20:22:55 -0700

    Added runInBackground

---
+++ Assets/Scripts/RunInBackground.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class RunInBackground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	  Application.runInBackground = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
